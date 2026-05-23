41b340e1884cb47514963e2d3a4bf1c696a4b413
unknown <vijayavasanthi mahisarla>
2026-05-23 21:22:28 +0530

    purebyte

---
+++ BACKEND/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Pure');
+})
+
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID is ${userId}`);
+});
+module.exports = app;
+
+const app = require('./app');
+const PORT = process.env.PORT || 3000;  
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
