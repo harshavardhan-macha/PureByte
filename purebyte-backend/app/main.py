fd56672b128cd63e2c296a565c7ac2d50e0237e8
harshavardhan-macha <harshavardhanmacha19@gmail.com>
2026-07-17 11:27:04 +0530

    added the frontend cors

--- purebyte-backend/app/main.py
+++ purebyte-backend/app/main.py
@@ -15,6 +15,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
